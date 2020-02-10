03113a65e2a6ca7687f9d5633efdb4f9ec2372fe
Виталик <vitalik@MacBook-Air-Zlata.local>
2020-02-10 19:39:38 +0200

    bugs

--- 14.php
+++ 14.php
@@ -1,5 +1,10 @@
 <?php
 
 
-$handly = fopen("/new_file.txt", "r");
-$mark = fgets($handly);
+$handly = fopen("/new_file.rtf", "r");
+$mark = [fgets($handly)];
+
+//$fizz = "F";
+//$buzz = "B";
+
+var_dump($mark);
